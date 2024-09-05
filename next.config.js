2d3eac4773b829f1a646ac75d434513170b57751
amber <117717748+joinamber@users.noreply.github.com>
2024-09-05 15:51:39 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Enables strict mode for React, which helps catch issues early
+  swcMinify: true, // Enables Next.js's faster SWC minifier for better performance
+}
+
+module.exports = nextConfig;
